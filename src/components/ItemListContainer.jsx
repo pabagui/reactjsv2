520a62c9eb2f5a6d7d16eb46c67df85d5b854ad8
pabagui <aguila.pablo@gmail.com>
2022-01-16 17:40:53 -0300

    desafios anteriores y bootstrap para react

---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const ItemListContainer = ( {greetings}) => {
+    // console.log(props) == console.log( {greetings} ), es la call back con destructuring de la props
+    return (
+        <div>
+            <h1>ITEMLIST</h1>
+            <p>{ greetings }</p>
+            
+        </div>
+    )
+}
---
+++ src/components/NavBar/CartWidget/CartWidget.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const CartWidget = () => {
+    return (
+        <div className="w-25">
+            <img src="/cesta-de-la-compra.png" alt="cesta de compras" className="w-25"/>
+        </div>
+    )
+}
